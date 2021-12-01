652712cfe80942701d756ce4496f6de130d49eb5
baypit <baypit2@hotmail.com>
2021-11-30 23:52:14 -0500

    listar productos
    listar productos

---
+++ src/app/Producto/listar/listar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../../Service/service.service';
+import { Router } from '@angular/router';
+import { Producto } from 'src/app/Modelo/Producto';
+
+@Component({
+  selector: 'app-listar',
+  templateUrl: './listar.component.html',
+  styleUrls: ['./listar.component.css']
+})
+export class ListarComponent implements OnInit {
+
+  productos:Producto[] | undefined;
+  constructor(private service:ServiceService, private router:Router ) { }
+
+  ngOnInit(): void {
+    this.service.getProductos().subscribe(data =>{
+      this.productos=data;
+    })
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ListarComponent } from './Producto/listar/listar.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kardex';
+
+  constructor(private router:Router){
+    
+  }
+
+  listar(){
+   this.router.navigate(["listar"]);   
+  }
+
+  nuevo(){
+    this.router.navigate(["add"]);   
+   }
+
 }
